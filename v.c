47892f12c7b98ed6b68a238c4c106bf40f76ac60
kirithicute <kirithibaskar@gmail.com>
2016-09-13 21:27:32 +0530

    Create v.c

---
+++ v.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+char cha[20]={2,7,11,15),;
+int a;
+printf("Enter the target value:");
+scanf("%c",&a);
+str(cha[0]+cha[1]);
+a=cha[0]+cha[1];
+printf("Enter the target :%d",a);
+return 0;
+}
